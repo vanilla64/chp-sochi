e8cf78a0d46b2e948c1f36e49b4c089e4f1e8626
Sergey <vanillaen@yandex.ru>
2023-07-20 03:00:47 +0400

    feat/final editing

--- src/hooks/useResize.ts
+++ src/hooks/useResize.ts
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 
 const useResize = () => {
-  // const [width, setWidth] = useState<number>(global?.window && window.innerWidth)
   const [width, setWidth] = useState<number>(0)
 
   const resizeHandler = () => setWidth(window.innerWidth)
 
   useEffect(() => {
     window.addEventListener('resize', resizeHandler)
-    resizeHandler() //TODO WORKS?
+    resizeHandler()
     return () => window.removeEventListener('resize', resizeHandler)
   }, [])
 
